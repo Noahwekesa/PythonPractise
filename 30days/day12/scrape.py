6dcf3b626a5deb26e16d4bc4eb98e57cb96a9f83
noahwekesa <noahwekesa0@gmail.com>
2024-04-12 02:26:23 +0300

    scraping

---
+++ 30days/day12/scrape.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def url_to_file(url, filename="world.html"):
+    r = requests.get(url)
+    if r.status_code == 200:
+        html_text = r.text
+        with open(filename, "w") as f:
+            f.write(html_text)
+        return html_text
+
+
+url = "https://www.boxofficemojo.com/year/world/"
+
+
+url_to_file(url)
