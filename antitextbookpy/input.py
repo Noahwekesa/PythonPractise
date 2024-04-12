4746166d9c983df6b4d69de3b520cba908f94c13
noahwekesa <noahwekesa0@gmail.com>
2024-04-12 17:52:03 +0300

    g

--- antitextbookpy/input.py
+++
@@ -1,6 +0,0 @@
-name =input("enter your name: ")
-age = input("enter your age: ")
-height = input("enter the your height: ")
-
-
-print("{} is {} years old and his height is {} cm".format(name, age, height))
--- antitextbookpy/variables.py
+++
@@ -1,3 +0,0 @@
-i = 1
-# id(object) - Return the “identity” of an object
-print("{} is the memory allocation of {}".format(id(i), i))
